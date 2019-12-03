3f30e2fa2b137ce94dd09dc8ad150c0142ff7b89
kewalg <kgulve1@binghamton.edu>
2019-12-03 10:35:26 -0500

    working on listview

--- app/src/main/java/data/CustomListViewAdapter.java
+++ app/src/main/java/data/CustomListViewAdapter.java
@@ -1,15 +1,57 @@
 package data;
+
+import android.app.Activity;
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+
 import model.*;
 
-public class CustomListViewAdapter extends ArrayAdapter<Food>{
+public class CustomListViewAdapter extends ArrayAdapter<Food> {
 
-    public CustomListViewAdapter(@NonNull Context context, int resource, int textViewResourceId) {
-        super(context, resource, textViewResourceId);
+    private int layoutResources;
+    private Activity activity;
+    private ArrayList<Food> foodlist = new ArrayList<>();
+
+    public CustomListViewAdapter(Activity act, int resource, ArrayList<Food> data) {
+        super(act, resource, data);
+        layoutResources = resource;
+        activity = act;
+        foodlist = data;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return super.getCount();
+    }
+
+    @Nullable
+    @Override
+    public Food getItem(int position) {
+        return super.getItem(position);
+    }
+
+    @Override
+    public int getPosition(@Nullable Food item) {
+        return super.getPosition(item);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return super.getItemId(position);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
     }
 }
