cf1307c4e8b1a834cce96dade6f4f24594a1f788
kewalg <kgulve1@binghamton.edu>
2019-12-02 22:38:22 -0500

    Added food class file under model for database

--- app/src/main/java/com/example/caloriestracker/MainActivity.java
+++ app/src/main/java/com/example/caloriestracker/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.caloriestracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edt_food = findViewById(R.id.edt_food);
+    EditText edt_cal = findViewById(R.id.edt_calories);
+    Button btn_submit = findViewById(R.id.submit_btn);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, FoodList.class);
+                startActivity(i);
+            }
+        });
     }
 }
