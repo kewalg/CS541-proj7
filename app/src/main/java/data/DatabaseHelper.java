4cd229ce2ee92a5954b61d363f49dc8b0a15b760
kewalg <kgulve1@binghamton.edu>
2019-12-04 20:37:32 -0500

    create new table on upgrade

--- app/src/main/java/data/DatabaseHelper.java
+++ app/src/main/java/data/DatabaseHelper.java
@@ -22,5 +22,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + Constants.TABLE_NAME);
+
+        onCreate(db);
     }
 }
