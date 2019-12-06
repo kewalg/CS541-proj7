65bd1d3131d197cd5f5afd3ab71b970f311ca3fa
kewalg <kgulve1@binghamton.edu>
2019-12-06 09:46:03 -0500

    added the food detail activity

--- app/src/main/java/com/example/caloriestracker/FoodDetail.java
+++ app/src/main/java/com/example/caloriestracker/FoodDetail.java
@@ -3,12 +3,33 @@ package com.example.caloriestracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import model.Food;
 
 public class FoodDetail extends AppCompatActivity {
+
+
+    private TextView foodName, calories, dateTaken;
+    private int foodID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_detail);
+
+
+        foodName = findViewById(R.id.tv_foodname);
+        calories = findViewById(R.id.tv_foodcal);
+        dateTaken = findViewById(R.id.tv_date);
+
+        Food food = (Food) getIntent().getSerializableExtra("userObj");
+        foodName.setText(food.getFoodName());
+        calories.setText(String.valueOf(food.getCalories()));
+        dateTaken.setText(food.getRecordDate());
+        foodID = food.getFoodId();
+
+
     }
 }
